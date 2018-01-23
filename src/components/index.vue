9799bd600a4c712fb1969654ac3116090bcabaae
jzhanglover <1292824917@qq.com>
2018-01-23 16:39:31 +0800

    分页组件

---
+++ src/components/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div align="right" style="margin-top: 30px">
+      <mo-pagination
+        :page-index="currentPage"
+        :total="count"
+        :page-size="pageSize"
+        @change="pageChange">
+      </mo-pagination>
+    </div>
+  </div>
+</template>
+<script>
+import moPagination from './pagination.vue'
+export default {
+  components: {moPagination},
+  data () {
+    return {
+      pageSize: 3, // 每页显示20条数据
+      currentPage: 1, // 当前页码
+      count: 10 // 总记录数,
+    }
+  },
+  methods: {
+    getList () {},
+    pageChange (index) {
+      this.currentPage = index
+      this.getList()
+    }
+  }
+}
+</script>
+<style scoped></style>
